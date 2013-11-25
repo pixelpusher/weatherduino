cb7cd7bdb44eaaccadd1d4924ca4522083b47b89
pixelpusher <evan@flkr.com>
2013-11-25 18:22:40 +0000

    added simple starting example

---
+++ start.js
@@ -0,0 +1,29 @@
+var five  = require("johnny-five");
+var board = new five.Board();
+var led;
+
+
+board.on("ready", function() {
+	led = new five.Led(13);
+  	var  button = new five.Button(2);
+
+  // Button Event API
+
+  // "down" the button is pressed
+  button.on("down", function() {
+	console.log("down");
+  });
+  
+  
+  // "hold" the button is pressed for specified time.
+  //        defaults to 500ms (1/2 second)
+  //        set
+  button.on("hold", function() {
+    console.log("hold");
+  });
+
+  // "up" the button is released
+  button.on("up", function() {
+    console.log("up");
+  });
+});
