e1c0288e0770af082ea6ee252ffef943e1a51a74
pixelpusher <evanraskob@gmail.com>
2015-02-05 18:33:54 +0000

    simpler and more informative arduino ports list

--- list-arduinos.js
+++ list-arduinos.js
@@ -1,35 +1,38 @@
+/**
+ * Find all serial ports that look like Arduinos and print them to the console.
+ * 
+ * by Evan Raskob info@pixelist.info 2015
+ * 
+ **/
+
 var serialport = require("serialport");
-//var __ = require("./lib/fn.js");
+
 var rport = /usb|acm|com/i;
-
-// adapted from the board.js file in Johnny-five
-// https://github.com/rwaldron/johnny-five/wiki/Board
-
 
 serialport.list(function(err, result) {
   var ports,
       length;
   
-  console.log(result);
-  //!rport.test(val.comName)
+  var i=0;
   
-/*
-  ports = result.filter(function(val) {
-    var available = true;
-
-    // Match only ports that Arduino cares about
-    // ttyUSB#, cu.usbmodem#, COM#
-    if ( !rport.test(val.comName) ) {
-      available = false;
-    }
-
-    return available;
-  }).map(function(val) {
-    return val.comName;
-  });
-
+  while (i < result.length)
+  {
+    console.log("found potential port:" + result[i]);
+    console.log();
+    
+    var portName = result[i].comName(val.comName);
+    
+     if (rport.test(portName))
+     {
+       // found a potential Arduino port
+       ports.push(portName);
+     }
+     // next result
+     i++;
+  }  
+  
   length = ports.length;
-*/
+
   // If no ports are detected when scanning /dev/, then there is
   // nothing left to do and we can safely exit the program
   if ( !length ) {
